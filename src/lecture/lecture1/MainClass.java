bbe87041549967ce46cb186be573925cf0c77f29
Arthur Molnar <arthur486@gmail.com>
2023-10-03 15:16:30 +0300

    Am afisat parametri liniei de comanda.

--- src/lecture/lecture1/MainClass.java
+++ src/lecture/lecture1/MainClass.java
@@ -4,6 +4,17 @@ public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Salut lume!");
+        System.out.println("Avem " + args.length + " parametri in linia de comanda");
+
+        String results = "";
+
+        for (int i = 0; i < args.length; i++) {
+//            System.out.print(args[i]);
+            results += args[i];
+        }
+
+        System.out.println("Concatenarea parametrilor: " + results);
+
     }
 
 }
