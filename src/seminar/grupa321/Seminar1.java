f9bde71bbd7b5a1d53f670f5f486a41e5e06e92a
Arthur Molnar <arthur486@gmail.com>
2023-10-03 16:38:19 +0300

    Primul commit la seminar :)

---
+++ src/seminar/grupa321/Seminar1.java
@@ -0,0 +1,15 @@
+package seminar.grupa321;
+
+/*
+    1. Sortam numerele transmise programului la linia de comanda. Daca programul e pornit fara parametri, citim numerele
+    de la tastatura.
+    2. Afisam numerele prime transmise programului la linia de comanda. Daca programul e pornit fara parametri, citim numerele
+    de la tastatura.
+    3. Facem un meniu ca sa alegem intre 1 si 2
+ */
+
+public class Seminar1 {
+    public static void main(String[] args) {
+        System.out.println("seminar 1");
+    }
+}
