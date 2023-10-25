345f743d990d7fa7665d4be5a2243b0bc1f564f1
Arthur Molnar <arthur486@gmail.com>
2023-10-25 13:37:37 +0300

    group 322 seminar 2 -- complete

--- src/seminar/grupa322/seminar2/Main.java
+++ src/seminar/grupa322/seminar2/Main.java
@@ -3,8 +3,13 @@ package seminar.grupa322.seminar2;
 import seminar.grupa322.seminar2.domain.Patient;
 import seminar.grupa322.seminar2.repository.Repository;
 
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Repository<Patient> repository = new Repository<Patient>();
         Patient pacient = new Patient(1, "a b", 1);
         Patient pacient1 = new Patient(2, "d b", 1);
@@ -17,9 +22,23 @@ public class Main {
         repository.add(pacient3);
         repository.add(pacient4);
 
+        FileWriter fw = new FileWriter("patient.txt");
+        int index = 0;
         for (var pacientIn : repository) {
             System.out.println(pacientIn);
+            // 1. Fisiere text
+            // Input/Output Stream -- fisiere binare
+            // Reader/Writer -- fisiere text
+            fw.write(pacientIn.toString());
+            fw.write("\n");
+
+            // 2. Serializarea
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("patient_" + index + ".binary"));
+            oos.writeObject(pacientIn);
+            oos.close();
+            index += 1;
         }
+        fw.close();
 
     }
 }
--- src/seminar/grupa322/seminar2/domain/Patient.java
+++ src/seminar/grupa322/seminar2/domain/Patient.java
@@ -1,6 +1,11 @@
 package seminar.grupa322.seminar2.domain;
 
-public class Patient extends Entity {
+import java.io.Serializable;
+
+public class Patient extends Entity implements Serializable {
+
+    // TODO Change value when class is updated
+    private static final long serialVersionUID = 1L;
 
     private String name;
     private int age;
